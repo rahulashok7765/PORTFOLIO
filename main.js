b31ac4b631e231c3ceb9855fc446bd2d76b5ac2c
rahulashok7765 <164659482+rahulashok7765@users.noreply.github.com>
2024-08-20 08:56:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new typed(".text", {
+    strings: ["JavaScript", "Frontend design", "Backend", "Database"],
+    typespeed: 100,
+    backspeed: 100,
+    backdelay: 1000,
+    loop: true
+
+});
